8919bfcc3bcbc966d09fcc396318f4ef2eb0665b
virginiawilkerson <vbw209@nyu.edu>
2019-02-14 12:48:33 -0500

    Added Quit and StartGame functions to MenuScript

---
+++ CodeLabWeek2_HW/Assets/Scripts/GameManger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    private int score = 0; //private = not available in the inspector 
+
+    public static GameManger instance;
+
+    public int Score //Property's are for getting and setting a value'
+    {
+        get { return score; }
+        set
+        {
+            score = value;
+
+            if (score > 10)
+            {
+                //Score = 10; //sets the maximum score to 10.  
+            }
+
+            if (score < 0)
+            {
+                score = 0; //sets the minimum score to 0 if there were a way to get a negative score. 
+            }
+            
+            print("Score now equals " + score); //once you finish using a message to help with your code, comment it out 
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null) //if this game object is uninitialized, dont destroy this game object and set the "instance" to "this"
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this; //"this" means this instance of a script I am currently in 
+            //if you havent (null) seen a Game Manager script, dont get rid of it. 
+            //if a gamemanager script DOES exist, destroy any duplicates 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Score ++;
+        //print("Your current score is: " + Score);
+    }
+    
+    
+    
+}
---
+++ CodeLabWeek2_HW/Assets/Scripts/HugScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HugScript : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)    //doesnt return a value unlike int and float //Triggers are not collidable, Collision is effected by gravity and physics 
+    {
+        Debug.Log("HIT!");
+        SceneManager.LoadScene(1);
+        GameManger.instance.Score++; //go into gameManager script and change the score 
+        //So now the score will carry over no matter how many times we load the scene 
+    }
+    
+}
